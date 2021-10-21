471eca0776ba01f9310eed7d3fccdb33bd33a891
Haesal <A01376509@itesm.mx>
2021-10-21 18:47:04 -0500

    Examen terminado

--- controllers/lugar.js
+++ controllers/lugar.js
@@ -1,30 +1,38 @@
 const Lugar = require("../models/lugar");
 const mongoose = require("mongoose")
 
-exports.postAgregarLugar = (req, res) => {
-    Lugar.findOne({lugar: req.body.lugar, pais: req.body.pais}, async function(err, lu){
-            if(err){
-                console.log(err);
-                res.send({ operacion: "incorrecta" })
-            }
-            if(lu){
-                try{
-                    const condicion = {lugar: lu.lugar, pais: lu.pais};
-                    const inte = {interes: lu.interes++};
-                    await Lugar.findOneAndUpdate(condicion, inte);
-                    res.send({ operacion: "Aumenta el interes del lugar" });
-                }catch(err){
-                    console.log(err);
-                    res.send({ operacion: "incorrecta" })
-                }
-            } else{
-                const lugar = new Lugar(req.body);
-                lugar._id = new mongoose.Types.ObjectId();
-                lugar.save();
-                console.log("Lugar registrado");
-                res.send({ operacion: "correcta" })
-            }
-    });
+exports.postAgregarLugar = async (req, res) => {
+    var unico = true;
+    const condicion = { lugar: req.body.lugar, pais: req.body.pais };
+    var inte = {};
+    await Lugar.findOne({ lugar: req.body.lugar, pais: req.body.pais }, function (err, lu) {
+        if (err) {
+            console.log(err);
+            res.send({ operacion: "incorrecta" })
+        }
+        if (lu) {
+            const update = lu.interes+1;
+            inte = {interes: update};
+            unico = false;
+        } else {
+            const lugar = new Lugar(req.body);
+            lugar._id = new mongoose.Types.ObjectId();
+            lugar.save();
+            console.log("Lugar registrado");
+            res.send({ operacion: "correcta" })
+        }
+    }).clone();
+
+    if (!unico) {
+        try {
+            await Lugar.findOneAndUpdate(condicion, inte);
+            unico = true;
+            res.send({ operacion: "Aumenta el interes del lugar" });
+        } catch (err) {
+            console.log(err);
+            res.send({ operacion: "incorrecta" })
+        }
+    }
 }
 
 exports.getObtenerLugar = async (req, res) => {
